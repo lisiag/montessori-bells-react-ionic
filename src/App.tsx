5949a0f63891c4768223d0d139b0bbb56fce8c81
Lisia Grocott <lisiagrocott@gmail.com>
2020-07-25 16:50:34 +1200

    play sound!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
   IonIcon,
@@ -8,31 +8,31 @@ import {
   IonTabBar,
   IonTabButton,
   IonTabs
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import bell_match_1 from './pages/bell_match_1';
-import bell_match_3 from './pages/bell_match_3';
-import bells from './pages/bells';
+} from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import { ellipse, square, triangle } from "ionicons/icons";
+import bell_match_1 from "./pages/bell_match_1";
+import { Bell_match_3 } from "./pages/bell_match_3";
+import bells from "./pages/bells";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 const App: React.FC = () => (
   <IonApp>
@@ -40,9 +40,13 @@ const App: React.FC = () => (
       <IonTabs>
         <IonRouterOutlet>
           <Route path="/bell_match_1" component={bell_match_1} exact={true} />
-          <Route path="/bell_match_3" component={bell_match_3} exact={true} />
+          <Route path="/bell_match_3" component={Bell_match_3} exact={true} />
           <Route path="/bells" component={bells} />
-          <Route path="/" render={() => <Redirect to="/bell_match_1" />} exact={true} />
+          <Route
+            path="/"
+            render={() => <Redirect to="/bell_match_1" />}
+            exact={true}
+          />
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="bell_match_1" href="/bell_match_1">
--- src/pages/bell_match_3.tsx
+++ src/pages/bell_match_3.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './bell_match_3.css';
+import React from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import "./bell_match_3.css";
+import ReactHowler from "react-howler";
 
-const bell_match_3: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Match bells (3)</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Match bells (3)</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-      </IonContent>
-    </IonPage>
-  );
-};
-
-export default bell_match_3;
+export class Bell_match_3 extends React.Component {
+  render() {
+    return (
+      <ReactHowler
+        src="http://goldfirestudios.com/proj/howlerjs/sound.ogg"
+        playing={true}
+      />
+    );
+  }
+}
