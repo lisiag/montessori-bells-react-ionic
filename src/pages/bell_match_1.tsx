c7aeb3e40c7e857f1307cea5756d9fc8e58a9251
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-01 11:25:13 +1200

    arrange bells in grid

--- src/pages/bell_match_1.tsx
+++ src/pages/bell_match_1.tsx
@@ -1,13 +1,14 @@
 import {
-  IonContent,
-  IonHeader,
-  IonItem,
-  IonLabel,
-  IonList,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  IonIcon
+    IonContent,
+    IonHeader,
+    IonItem,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonIcon
 } from "@ionic/react";
 import { notifications } from "ionicons/icons";
 import React from "react";
@@ -16,32 +17,86 @@ import { Howl, Howler } from "howler";
 
 const labelRef = React.createRef<HTMLDivElement>();
 const a4 = new Howl({
-  src: ["../../assets/sounds/pianoA4.mp3"]
+    src: ["../../assets/sounds/pianoA4.mp3"]
 });
 
 const bell_match_1: React.FC = () => {
-  return (
-    <IonPage ref={labelRef}>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Match bell first title</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonList>
-          <IonItem
-            button
-            onClick={() => {
-              a4.play();
-            }}
-          >
-            <IonIcon class="icon" icon={notifications} />
-          </IonItem>
-          <IonLabel id="testLabel">Change me.</IonLabel>
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+    return (
+        <IonPage ref={labelRef}>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Match bell first title</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent className="ion-padding">
+                <IonGrid>
+                    <IonRow>
+                        <IonCol></IonCol>
+                        <IonCol></IonCol>
+                        <IonCol>
+                            <IonItem
+                                button
+                                onClick={() => {
+                                    a4.play();
+                                }}
+                            >
+                                <IonIcon
+                                    class="right_icon"
+                                    icon={notifications}
+                                />
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem
+                                button
+                                onClick={() => {
+                                    a4.play();
+                                }}
+                            >
+                                <IonIcon
+                                    class="left_icon"
+                                    icon={notifications}
+                                />
+                            </IonItem>
+                        </IonCol>
+                        <IonCol></IonCol>
+                        <IonCol>
+                            <IonItem
+                                button
+                                onClick={() => {
+                                    a4.play();
+                                }}
+                            >
+                                <IonIcon
+                                    class="right_icon"
+                                    icon={notifications}
+                                />
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol></IonCol>
+                        <IonCol></IonCol>
+                        <IonCol>
+                            <IonItem
+                                button
+                                onClick={() => {
+                                    a4.play();
+                                }}
+                            >
+                                <IonIcon
+                                    class="right_icon"
+                                    icon={notifications}
+                                />
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                </IonGrid>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default bell_match_1;
