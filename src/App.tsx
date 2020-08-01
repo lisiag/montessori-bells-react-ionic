1979ab5ce11314783ba1939a84d7c3d13f65d7b9
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-01 16:31:32 +1200

    convert to bell_match_1 to class BellMatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import {
-  IonApp,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs
+    IonApp,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+    IonTabBar,
+    IonTabButton,
+    IonTabs
 } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 import { ellipse, square, triangle } from "ionicons/icons";
-import bell_match_1 from "./pages/bell_match_1";
+import { BellMatch } from "./pages/bell_match_1";
 import bell_match_3 from "./pages/bell_match_3";
 import bells from "./pages/bells";
 
@@ -35,36 +35,44 @@ import "@ionic/react/css/display.css";
 import "./theme/variables.css";
 
 const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-      <IonTabs>
-        <IonRouterOutlet>
-          <Route path="/bell_match_1" component={bell_match_1} exact={true} />
-          <Route path="/bell_match_3" component={bell_match_3} exact={true} />
-          <Route path="/bells" component={bells} />
-          <Route
-            path="/"
-            render={() => <Redirect to="/bell_match_1" />}
-            exact={true}
-          />
-        </IonRouterOutlet>
-        <IonTabBar slot="bottom">
-          <IonTabButton tab="bell_match_1" href="/bell_match_1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Match bell</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="bell_match_3" href="/bell_match_3">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Match bells (3)</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="bells" href="/bells">
-            <IonIcon icon={square} />
-            <IonLabel>Bells</IonLabel>
-          </IonTabButton>
-        </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-  </IonApp>
+    <IonApp>
+        <IonReactRouter>
+            <IonTabs>
+                <IonRouterOutlet>
+                    <Route
+                        path="/bell_match_1"
+                        component={BellMatch}
+                        exact={true}
+                    />
+                    <Route
+                        path="/bell_match_3"
+                        component={bell_match_3}
+                        exact={true}
+                    />
+                    <Route path="/bells" component={bells} />
+                    <Route
+                        path="/"
+                        render={() => <Redirect to="/bell_match_1" />}
+                        exact={true}
+                    />
+                </IonRouterOutlet>
+                <IonTabBar slot="bottom">
+                    <IonTabButton tab="bell_match_1" href="/bell_match_1">
+                        <IonIcon icon={triangle} />
+                        <IonLabel>Match bell</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="bell_match_3" href="/bell_match_3">
+                        <IonIcon icon={ellipse} />
+                        <IonLabel>Match bells (3)</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="bells" href="/bells">
+                        <IonIcon icon={square} />
+                        <IonLabel>Bells</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+            </IonTabs>
+        </IonReactRouter>
+    </IonApp>
 );
 
 export default App;
--- src/pages/bell_match_1.tsx
+++ src/pages/bell_match_1.tsx
@@ -16,91 +16,121 @@ import "./bell_match_1.css";
 import { Howl } from "howler";
 import Draggable from "react-draggable";
 
-const labelRef = React.createRef<HTMLDivElement>();
-const a4 = new Howl({
-    src: ["../../assets/sounds/pianoA4.mp3"]
-});
+export class BellMatch extends React.Component {
+    labelRef: any;
+    constructor(props: any) {
+        super(props);
+        this.labelRef = React.createRef<HTMLDivElement>();
+    }
 
-const bell_match_1: React.FC = () => {
-    return (
-        <IonPage ref={labelRef}>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Match bell first title</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent className="ion-padding">
-                <IonGrid>
-                    <IonRow>
-                        <IonCol></IonCol>
-                        <IonCol></IonCol>
-                        <IonCol>
-                            <IonItem
-                                button
-                                onClick={() => {
-                                    a4.play();
-                                }}
-                            >
-                                <IonIcon
-                                    class="right_icon"
-                                    icon={notifications}
-                                />
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                    <IonRow>
-                        <IonCol>
-                            <Draggable
-                                defaultClassNameDragging=""
-                                defaultClassNameDragged=""
-                                grid={[100, 100]}
-                                onStop={ev => {
-                                    console.log(ev);
-                                    a4.play();
-                                }}
-                            >
-                                <IonIcon
-                                    className="left_icon"
-                                    icon={notifications}
-                                />
-                            </Draggable>
-                        </IonCol>
-                        <IonCol></IonCol>
-                        <IonCol>
-                            <IonItem
-                                button
-                                onClick={() => {
-                                    a4.play();
-                                }}
-                            >
-                                <IonIcon
-                                    class="right_icon"
-                                    icon={notifications}
-                                />
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                    <IonRow>
-                        <IonCol></IonCol>
-                        <IonCol></IonCol>
-                        <IonCol>
-                            <IonItem
-                                button
-                                onClick={() => {
-                                    a4.play();
-                                }}
-                            >
-                                <IonIcon
-                                    class="right_icon"
-                                    icon={notifications}
-                                />
-                            </IonItem>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    );
-};
+    a4 = new Howl({
+        src: ["../../assets/sounds/pianoA4.mp3"]
+    });
 
-export default bell_match_1;
+    state = {
+        deltaPosition: {
+            x: 0,
+            y: 0
+        }
+    };
+
+    handleDrag = (e: any, ui: any) => {
+        this.setState({
+            deltaPosition: {
+                x: ui.deltaX,
+                y: ui.deltaY
+            }
+        });
+    };
+
+    render() {
+        return (
+            <IonPage ref={this.labelRef}>
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Match bell first title</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonContent className="ion-padding">
+                    <IonGrid>
+                        <IonRow>
+                            <IonCol></IonCol>
+                            <IonCol></IonCol>
+                            <IonCol>
+                                <IonItem
+                                    button
+                                    onClick={() => {
+                                        this.a4.play();
+                                    }}
+                                >
+                                    <IonIcon
+                                        class="right_icon"
+                                        icon={notifications}
+                                    />
+                                </IonItem>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol>
+                                <Draggable
+                                    defaultClassNameDragging=""
+                                    defaultClassNameDragged=""
+                                    grid={[100, 100]}
+                                    onStart={() => {
+                                        this.state.deltaPosition.x = 0;
+                                        this.state.deltaPosition.y = 0;
+                                    }}
+                                    onStop={ev => {
+                                        console.log(ev);
+                                        if (
+                                            this.state.deltaPosition.x == 0 &&
+                                            this.state.deltaPosition.y == 0
+                                        ) {
+                                            this.a4.play();
+                                        }
+                                    }}
+                                >
+                                    <IonIcon
+                                        className="left_icon"
+                                        icon={notifications}
+                                    />
+                                </Draggable>
+                            </IonCol>
+                            <IonCol></IonCol>
+                            <IonCol>
+                                <IonItem
+                                    button
+                                    onClick={() => {
+                                        this.a4.play();
+                                    }}
+                                >
+                                    <IonIcon
+                                        class="right_icon"
+                                        icon={notifications}
+                                    />
+                                </IonItem>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol></IonCol>
+                            <IonCol></IonCol>
+                            <IonCol>
+                                <IonItem
+                                    button
+                                    onClick={() => {
+                                        this.a4.play();
+                                    }}
+                                >
+                                    <IonIcon
+                                        class="right_icon"
+                                        icon={notifications}
+                                    />
+                                </IonItem>
+                            </IonCol>
+                        </IonRow>
+                    </IonGrid>
+                </IonContent>
+            </IonPage>
+        );
+    }
+}
