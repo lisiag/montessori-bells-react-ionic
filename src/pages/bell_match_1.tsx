5cacdc49f81299a2e5a541703f4e54a161516e29
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-01 21:16:13 +1200

    refactor: move some to util; replace IonItems with Draggables

--- src/pages/bell_match_1.tsx
+++ src/pages/bell_match_1.tsx
@@ -13,7 +13,7 @@ import {
 import { notifications } from "ionicons/icons";
 import React from "react";
 import "./bell_match_1.css";
-import { Howl } from "howler";
+import { Util } from "../util";
 import Draggable from "react-draggable";
 
 export class BellMatch extends React.Component {
@@ -23,69 +23,10 @@ export class BellMatch extends React.Component {
         this.labelRef = React.createRef<HTMLDivElement>();
     }
 
-    c4 = new Howl({
-        src: ["../../assets/sounds/pianoC4.mp3"]
-    });
-    d4 = new Howl({
-        src: ["../../assets/sounds/pianoD4.mp3"]
-    });
-    e4 = new Howl({
-        src: ["../../assets/sounds/pianoE4.mp3"]
-    });
-    f4 = new Howl({
-        src: ["../../assets/sounds/pianoF4.mp3"]
-    });
-    g4 = new Howl({
-        src: ["../../assets/sounds/pianoG4.mp3"]
-    });
-    a4 = new Howl({
-        src: ["../../assets/sounds/pianoA4.mp3"]
-    });
-    b4 = new Howl({
-        src: ["../../assets/sounds/pianoB4.mp3"]
-    });
-    c5 = new Howl({
-        src: ["../../assets/sounds/pianoC5.mp3"]
-    });
-
-    notes = [
-        this.c4,
-        this.d4,
-        this.e4,
-        this.f4,
-        this.g4,
-        this.a4,
-        this.b4,
-        this.c5
-    ];
-
-    /*
-       Get n random numbers between 0 (inclusive) and arrLen (exclusive).
-       Intended for use getting n random elements from an array of length arrLen.
-     */
-    getRandoms = (arrLen: number, n: number) => {
-        if (n > arrLen)
-            throw new RangeError(
-                "getRandoms: more elements taken than available"
-            );
-        let result = new Array(n);
-        let workArr = new Array(arrLen);
-        for (let i = 0; i < arrLen; ++i) {
-            workArr[i] = i;
-        }
-        for (let i = 0; i < n; ++i) {
-            let x = Math.floor(Math.random() * workArr.length);
-            result[i] = workArr[x];
-            // remove the number from workArr so it can't be selected again
-            workArr.splice(x, 1);
-        }
-        return result;
-    };
-
     /*
        Get indices for 3 random bells for the righthand column
      */
-    threeRandof8 = this.getRandoms(8, 3);
+    threeRandof8 = Util.getRandoms(8, 3);
 
     /*
        Pick a random bell from the three on the right, for the user to match
@@ -122,17 +63,17 @@ export class BellMatch extends React.Component {
                             <IonCol></IonCol>
                             <IonCol></IonCol>
                             <IonCol>
-                                <IonItem
-                                    button
-                                    onClick={() => {
-                                        this.notes[this.threeRandof8[0]].play();
+                                <Draggable
+                                    onStart={() => {
+                                        Util.notes[this.threeRandof8[0]].play();
+                                        return false;
                                     }}
                                 >
                                     <IonIcon
-                                        class="right_icon"
+                                        className="right_icon"
                                         icon={notifications}
                                     />
-                                </IonItem>
+                                </Draggable>
                             </IonCol>
                         </IonRow>
                         <IonRow>
@@ -155,7 +96,7 @@ export class BellMatch extends React.Component {
                                             this.state.deltaPosition.x === 0 &&
                                             this.state.deltaPosition.y === 0
                                         ) {
-                                            this.notes[this.oneRand].play();
+                                            Util.notes[this.oneRand].play();
                                         }
                                     }}
                                 >
@@ -167,37 +108,34 @@ export class BellMatch extends React.Component {
                             </IonCol>
                             <IonCol></IonCol>
                             <IonCol>
-                                <IonItem
-                                    button
-                                    onClick={() => {
-                                        this.notes[this.threeRandof8[1]].play();
+                                <Draggable
+                                    onStart={() => {
+                                        Util.notes[this.threeRandof8[1]].play();
+                                        return false;
                                     }}
                                 >
                                     <IonIcon
-                                        class="right_icon"
+                                        className="right_icon"
                                         icon={notifications}
                                     />
-                                </IonItem>
+                                </Draggable>
                             </IonCol>
                         </IonRow>
                         <IonRow>
                             <IonCol></IonCol>
                             <IonCol></IonCol>
                             <IonCol>
-                                <IonItem
-                                    button
-                                    onClick={() => {
-                                        this.notes[this.threeRandof8[2]].play();
-                                        for (const num of this.threeRandof8) {
-                                            console.log(num);
-                                        }
+                                <Draggable
+                                    onStart={() => {
+                                        Util.notes[this.threeRandof8[2]].play();
+                                        return false;
                                     }}
                                 >
                                     <IonIcon
-                                        class="right_icon"
+                                        className="right_icon"
                                         icon={notifications}
                                     />
-                                </IonItem>
+                                </Draggable>
                             </IonCol>
                         </IonRow>
                     </IonGrid>
---
+++ src/util.tsx
@@ -0,0 +1,55 @@
+import { Howl } from "howler";
+
+export const Util = {
+    /*
+       Get n random numbers between 0 (inclusive) and arrLen (exclusive).
+       Intended for use getting n random elements from an array of length arrLen.
+     */
+    getRandoms: (arrLen: number, n: number) => {
+        if (n > arrLen)
+            throw new RangeError(
+                "getRandoms: more elements taken than available"
+            );
+        let result = new Array(n);
+        let workArr = new Array(arrLen);
+        for (let i = 0; i < arrLen; ++i) {
+            workArr[i] = i;
+        }
+        for (let i = 0; i < n; ++i) {
+            let x = Math.floor(Math.random() * workArr.length);
+            result[i] = workArr[x];
+            // remove the number from workArr so it can't be selected again
+            workArr.splice(x, 1);
+        }
+        return result;
+    },
+
+    notes: [] as Howl[]
+};
+
+Util.notes = [
+    new Howl({
+        src: ["../../assets/sounds/pianoC4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoD4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoE4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoF4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoG4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoA4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoB4.mp3"]
+    }),
+    new Howl({
+        src: ["../../assets/sounds/pianoC5.mp3"]
+    })
+];
