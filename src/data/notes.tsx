5b560c1c5b203e6c797225221935303ef4b66f27
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-05 19:16:06 +1200

    refactor bells: build in render() not in constructor

--- src/data/notes.tsx
+++ src/data/notes.tsx
@@ -5,8 +5,10 @@ export class Notes {
 
     constructor() {
         this._notes = [];
+
         /* Names of the notes of the scale of C major from middle-C in descending order */
         const noteNames = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
+
         const colors = [
             "red",
             "darkorange",
@@ -18,6 +20,7 @@ export class Notes {
             "magenta"
         ];
         const extraColors = ["limegreen", "dodgerblue"];
+
         for (let i = 0; i < noteNames.length; ++i) {
             let soundLocation =
                 "../../assets/sounds/piano" + noteNames[i] + ".mp3";
--- src/ui/components/bells.tsx
+++ src/ui/components/bells.tsx
@@ -8,25 +8,20 @@ import {
     IonGrid,
     IonRow
 } from "@ionic/react";
-import React, { ReactNode } from "react";
+import React from "react";
 import "./bells.css";
 import { Toolbar } from "./toolbar";
 import { Bell } from "./bell";
 import { Util } from "../../business/util";
 
 export interface BellsProps {
-    numPairs: number;
-    numRows: number;
+    numPairs: number /* the number of pairs of bells for user to match; i.e. the number of bells in the lefthand col */;
+    numRows: number /* the number of rows of bells; the number of bells in the righthand column */;
     rref: any;
     instructions: string;
 }
 
 export class Bells extends React.Component<BellsProps> {
-    bells: ReactNode;
-    numPairs: number /* the number of pairs bells for user to match; the number of bells in the lefthand col */;
-    numRows: number; /* the number of rows of bells; the number of bells in the righthand column */
-    rref: any;
-    instructions: string;
     notes: Array<number>;
     notesSorted: Array<number>;
     indices: Array<number>;
@@ -34,9 +29,9 @@ export class Bells extends React.Component<BellsProps> {
     /* If there is only one bell on the left for the user to pair up with a bell on the right, place
        it in the second row; otherwise place a bell on the left in every row. Even though I want a
        random bell in the case of only one bell on the left, I always take the second one from the
-       "notes" array, but that's ok because notes in the array are in random order. */
+       "notes" array, but that's ok because the elements of that array are in random order. */
     renderCell(index: number) {
-        if (this.numPairs != 1 || index === 1) {
+        if (this.props.numPairs != 1 || index === 1) {
             return <Bell note={this.notes[index]} cls="left_icon" />;
         } else {
             return <IonCol></IonCol>;
@@ -45,15 +40,11 @@ export class Bells extends React.Component<BellsProps> {
 
     constructor(props: BellsProps) {
         super(props);
-        this.numPairs = props.numPairs;
-        this.numRows = props.numRows;
-        this.rref = props.rref;
-        this.instructions = props.instructions;
 
         /*
-       Get random notes for the bells in the righthand column
-     */
-        this.notes = Util.getRandoms(8, this.numRows);
+           Get random notes for the bells in the righthand column
+         */
+        this.notes = Util.getRandoms(8, this.props.numRows);
 
         /* Sort bells from high to low for the righthand column */
         this.notesSorted = this.notes
@@ -66,18 +57,20 @@ export class Bells extends React.Component<BellsProps> {
         for (let i = 0; i < this.notes.length; ++i) {
             this.indices.push(i);
         }
+    }
 
+    render() {
         /* Arrange the bells: on the left in random order; on the right sorted high to low
          */
-        this.bells = (
-            <IonPage ref={this.rref}>
+        return (
+            <IonPage ref={this.props.rref}>
                 <IonHeader>
                     <IonToolbar>
                         <IonTitle>Pair the matching bells</IonTitle>
                     </IonToolbar>
                 </IonHeader>
                 <IonContent className="ion-padding">
-                    <Toolbar instructions={this.instructions} />
+                    <Toolbar instructions={this.props.instructions} />
                     <IonGrid>
                         {this.indices.map(index => (
                             <IonRow>
@@ -93,9 +86,5 @@ export class Bells extends React.Component<BellsProps> {
                 </IonContent>
             </IonPage>
         );
-    }
-
-    render() {
-        return this.bells;
     }
 }
