7283abfe286680799f3bbaa8bbb32972f3da7e51
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-05 18:49:49 +1200

    add instructions popup

--- src/ui/components/toolbar.tsx
+++ src/ui/components/toolbar.tsx
@@ -1,12 +1,7 @@
-import {
-    IonToolbar,
-    IonButtons,
-    IonButton,
-    IonIcon,
-    IonLabel
-} from "@ionic/react";
-import React, { ReactNode } from "react";
-import { play, helpCircle, eye } from "ionicons/icons";
+import { IonButtons, IonButton, IonIcon, IonLabel } from "@ionic/react";
+import React from "react";
+import Modal from "react-modal";
+import { play, helpCircle, eye, close } from "ionicons/icons";
 import "./toolbar.css";
 import { Util } from "../../business/util";
 
@@ -14,43 +9,59 @@ export interface ToolbarProps {
     instructions: string;
 }
 
-export class Toolbar extends React.Component<ToolbarProps> {
-    toolbar: ReactNode;
-    instructions: string;
+export interface ToolbarState {
+    instructionsIsOpen: boolean;
+}
 
+export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
     constructor(props: ToolbarProps) {
         super(props);
-        this.instructions = props.instructions;
-
-        this.toolbar = (
-            <IonToolbar>
-                <IonButtons>
-                    <IonButton
-                        onClick={() => {
-                            Util.refreshPage();
-                        }}
-                    >
-                        <IonIcon icon={play} />
-                        <IonLabel>Play again</IonLabel>
-                    </IonButton>
-                    <IonButton
-                        onClick={() => {
-                            alert(this.instructions);
-                        }}
-                    >
-                        <IonIcon icon={helpCircle} />
-                        <IonLabel>Instructions</IonLabel>
-                    </IonButton>
-                    <IonButton onClick={() => {}}>
-                        <IonIcon icon={eye} />
-                        <IonLabel>Answers</IonLabel>
-                    </IonButton>
-                </IonButtons>
-            </IonToolbar>
-        );
+        this.state = { instructionsIsOpen: false };
     }
 
+    openInstructions = () => {
+        this.setState({ instructionsIsOpen: true });
+    };
+
+    closeInstructions = () => {
+        this.setState({ instructionsIsOpen: false });
+    };
+
     render() {
-        return this.toolbar;
+        return (
+            <IonButtons>
+                <IonButton
+                    onClick={() => {
+                        Util.refreshPage();
+                    }}
+                >
+                    <IonIcon icon={play} />
+                    <IonLabel>Play again</IonLabel>
+                </IonButton>
+
+                <IonButton onClick={this.openInstructions}>
+                    <IonIcon icon={helpCircle} />
+                    <IonLabel>Instructions</IonLabel>
+                </IonButton>
+
+                <Modal
+                    isOpen={this.state.instructionsIsOpen}
+                    onRequestClose={this.closeInstructions}
+                    shouldCloseOnOverlayClick={true}
+                >
+                    <div onClick={this.closeInstructions}>
+                        {this.props.instructions}
+                    </div>
+                    <IonButton onClick={this.closeInstructions}>
+                        <IonIcon icon={close}></IonIcon>
+                    </IonButton>
+                </Modal>
+
+                <IonButton onClick={() => {}}>
+                    <IonIcon icon={eye} />
+                    <IonLabel>Answers</IonLabel>
+                </IonButton>
+            </IonButtons>
+        );
     }
 }
