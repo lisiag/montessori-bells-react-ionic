f1a05e3a8f3c1dc932853a4d019e064adaf6c4f4
Lisia Grocott <lisiagrocott@gmail.com>
2020-08-05 21:09:47 +1200

    wip snap bell to align y, but currently aligns from too far away

--- src/ui/components/bell.tsx
+++ src/ui/components/bell.tsx
@@ -33,7 +33,27 @@ export class Bell extends React.Component<BellProps> {
     }
 
     alignYIfClose(ui: DraggableData): number {
-        return ui.y;
+        const bound = ui.node.getBoundingClientRect();
+        /* get all the bells in the right column */
+        const rightBells = ui.node
+            .closest("ion-grid")!
+            .getElementsByClassName("rightBell");
+
+        /* find which of the bells in the right column is closest */
+        let closest = rightBells[0];
+        let minDiff = 1000000;
+        for (let i = 0; i < rightBells.length; ++i) {
+            const rBellBound = rightBells[i].getBoundingClientRect();
+            const currentDiff = Math.abs(bound.top - rBellBound.top);
+            if (currentDiff < minDiff) {
+                minDiff = currentDiff;
+                closest = rightBells[i];
+            }
+        }
+
+        /* Adjust my y such that I align with the closest bell in the right column */
+        const diff = bound.top - closest.getBoundingClientRect().top;
+        return ui.y - diff;
     }
 
     render() {
@@ -68,20 +88,24 @@ export class Bell extends React.Component<BellProps> {
                                 const gridBound = grid.getBoundingClientRect();
                                 const bound = ui.node.getBoundingClientRect();
 
-                                if (bound.left < gridBound.left) {
-                                    /* snap bell back */
-                                    x = -LEFT_BOUND;
-                                } else if (
+                                if (
                                     bound.right >
                                     gridBound.right - 1.6 * bound.width
                                 ) {
                                     x = gridBound.right - 2.3 * bound.width;
                                     y = this.alignYIfClose(ui);
-                                }
-                                if (bound.top < gridBound.top) {
-                                    y = -TOP_BOUND;
-                                } else if (bound.bottom > gridBound.bottom) {
-                                    y += gridBound.bottom - bound.bottom;
+                                } else {
+                                    if (bound.left < gridBound.left) {
+                                        /* snap bell back */
+                                        x = -LEFT_BOUND;
+                                    }
+                                    if (bound.top < gridBound.top) {
+                                        y = -TOP_BOUND;
+                                    } else if (
+                                        bound.bottom > gridBound.bottom
+                                    ) {
+                                        y += gridBound.bottom - bound.bottom;
+                                    }
                                 }
                                 this.controlledPosition.x = x;
                                 this.controlledPosition.y = y;
@@ -106,7 +130,7 @@ export class Bell extends React.Component<BellProps> {
                         }}
                     >
                         <IonIcon
-                            className={this.props.cls}
+                            className={this.props.cls + " rightBell"}
                             icon={notifications}
                         />
                     </Draggable>
